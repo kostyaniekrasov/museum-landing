cd51876ef4d815bd0edd28a7a98c4874ca4ce202
Kostya Niekrasov <kostyanekrasov1501@gmail.com>
2024-06-11 15:04:30 +0300

    make build

---
+++ index.61909509.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("subscription__field");e.addEventListener("submit",function(t){t.preventDefault(),e.reset()});
+//# sourceMappingURL=index.61909509.js.map
---
+++ index.898726de.js
@@ -0,0 +1,2 @@
+var o=document.getElementById("top-button");window.onscroll=function(){document.body.scrollTop>600||document.documentElement.scrollTop>600?o.style.display="flex":o.style.display="none"},o.addEventListener("click",function(){document.body.scrollTop=0,document.documentElement.scrollTop=0});
+//# sourceMappingURL=index.898726de.js.map
